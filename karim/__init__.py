30dadd93ed816baf682453114187f26c6daba052
David Wicker <davidwickerhf@gmail.com>
2020-10-05 17:37:36 +0200

    Updated Imports

--- karim/__init__.py
+++ karim/__init__.py
@@ -4,25 +4,10 @@ import telegram
 from flask import Flask
 from telegram.utils.request import Request
 from karim.secrets import secrets
-from karim.bot import telebot
 from telegram.error import RetryAfter
 import time
 from karim.classes.teleredis import RedisSession
 import redis
-
-# Initialize Bot
-BOT_TOKEN = secrets.get_var('BOT_TOKEN')
-URL = secrets.get_var('SERVER_APP_DOMAIN')
-request = Request(con_pool_size=8)
-bot = telegram.Bot(token=BOT_TOKEN, request=request)
-update_queue = telebot.setup(bot)
-
-# Setup Telegram Webhook
-try:
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
-except RetryAfter:
-    time.sleep(4)
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
 
 # SET UP REDIS SESSION CONNECTOR
 LOCALHOST = True
@@ -35,6 +20,22 @@ if os.environ.get('PORT') in (None, ""):
 else:
     LOCALHOST = False
     redis_connector = redis.Redis(host=secrets.get_var('REDIS_URL'), port=14909, db=0, decode_responses=False)
+
+# Initialize Bot
+from karim.bot import telebot
+
+BOT_TOKEN = secrets.get_var('BOT_TOKEN')
+URL = secrets.get_var('SERVER_APP_DOMAIN')
+request = Request(con_pool_size=8)
+bot = telegram.Bot(token=BOT_TOKEN, request=request)
+update_queue = telebot.setup(bot)
+
+# Setup Telegram Webhook
+try:
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
+except RetryAfter:
+    time.sleep(4)
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
 
 
 # Initialize Flask App
