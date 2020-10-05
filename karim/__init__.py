8fe763b65ea6eb741c22abdb4e9226962757fd07
David Wicker <davidwickerhf@gmail.com>
2020-10-05 15:52:41 +0200

    Catch Webhook error

--- karim/__init__.py
+++ karim/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from telegram.utils.request import Request
 from karim.secrets import secrets
 from karim.bot import telebot
+from telegram.error import RetryAfter
+import time
 
 # Initialize Bot
 BOT_TOKEN = secrets.get_var('BOT_TOKEN')
@@ -12,7 +14,14 @@ bot = telegram.Bot(token=BOT_TOKEN, request=request)
 update_queue = telebot.setup(bot)
 
 # Setup Telegram Webhook
-s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
+print('LOGS: Bot Token: ', BOT_TOKEN)
+print('LOGS: URL: ', URL)
+try:
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
+except RetryAfter:
+    time.sleep(4)
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=BOT_TOKEN))
+
 
 # Initialize Flask App
 app = Flask(__name__)
