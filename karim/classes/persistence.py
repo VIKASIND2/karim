ec52b394651299e418957bcc3f12de5585aa7de3
David Wicker <davidwickerhf@gmail.com>
2020-10-13 16:09:24 +0200

    Update persistence.py

--- karim/classes/persistence.py
+++ karim/classes/persistence.py
@@ -1,7 +1,7 @@
 import json
 from karim import LOCALHOST
 from telegram import update
-import os, jsonpickle, redis
+import os, redis
 
 def persistence_decorator(func):
     def wrapper(self, *args, **kw):
