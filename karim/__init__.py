576bdb53a328148cfd6f7f8f7ffcc3be57e9005a
David Wicker <davidwickerhf@gmail.com>
2020-10-26 16:14:28 +0100

    Update __init__.py

--- karim/__init__.py
+++ karim/__init__.py
@@ -31,8 +31,8 @@ PORT = int(os.environ.get('PORT', 5000))
 # set connection pool size for bot 
 request = Request(con_pool_size=8)
 q = mq.MessageQueue(all_burst_limit=3, all_time_limit_ms=3000)
-testbot = MQBot(BOT_TOKEN, request=request, mqueue=q, defaults=Defaults(parse_mode=ParseMode.HTML))
-updater = Updater(bot=testbot, use_context=True, defaults=Defaults(parse_mode=ParseMode.HTML))
+testbot = MQBot(BOT_TOKEN, request=request, mqueue=q)
+updater = Updater(bot=testbot, use_context=True)
 
 # SET UP BOT COMMAND HANDLERS
 telebot.setup(updater)
