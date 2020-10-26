55ae647e405179f453d75146377733ede4b7712d
David Wicker <davidwickerhf@gmail.com>
2020-10-26 14:06:03 +0100

    Update __init__.py

--- karim/__init__.py
+++ karim/__init__.py
@@ -30,7 +30,7 @@ URL = secrets.get_var('SERVER_APP_DOMAIN')
 PORT = int(os.environ.get('PORT', 5000))
 # set connection pool size for bot 
 request = Request(con_pool_size=8)
-testbot = MQBot(BOT_TOKEN, request=request, mqueue=q, defaults=Defaults(parse_mode=ParseMode.HTML))
+testbot = MQBot(BOT_TOKEN, request=request, mqueue=mq, defaults=Defaults(parse_mode=ParseMode.HTML))
 updater = Updater(bot=testbot, use_context=True, defaults=Defaults(parse_mode=ParseMode.HTML))
 q = mq.MessageQueue(all_burst_limit=3, all_time_limit_ms=3000)
 # SET UP BOT COMMAND HANDLERS
