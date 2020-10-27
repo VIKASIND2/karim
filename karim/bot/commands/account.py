71a28caec142230a129f4386d5b6e0692ee47590
David Wicker <davidwickerhf@gmail.com>
2020-10-27 20:22:23 +0100

    debugging

--- karim/bot/commands/account.py
+++ karim/bot/commands/account.py
@@ -27,16 +27,16 @@ def check_account(update, context):
             markup = CreateMarkup({Callbacks.LOGOUT: 'Log Out (Telegram)'}).create_markup()
             manager.discard()
             instasession.discard()
-            context.bot.edit_message_text(text, reply_markup=markup, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.id)
+            context.bot.edit_message_text(text, reply_markup=markup, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.message_id)
         else:
             # User is not logged in
             markup = CreateMarkup({Callbacks.LOGIN: 'Log In'}).create_markup()
             manager.discard()
             instasession.discard()
-            context.bot.edit_message_text(no_connection, reply_markup=markup, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.id)
+            context.bot.edit_message_text(no_connection, reply_markup=markup, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.message_id)
     except:
         # Error
         manager.discard()
         instasession.discard()
-        context.bot.edit_message_text(problem_connecting, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.id)
+        context.bot.edit_message_text(problem_connecting, parse_mode=ParseMode.HTML, chat_id=update.effective_chat.id, message_id=message.message_id)
         
