8ed07f8d6710f8e74c16beb67fc127b0d3e0c9fd
David Wicker <davidwickerhf@gmail.com>
2020-11-11 19:16:47 +0100

    Update forward.py

--- karim/bot/commands/forward.py
+++ karim/bot/commands/forward.py
@@ -328,7 +328,9 @@ def confirm(update, context):
         elif forwarder.get_mode() == Callbacks.INSTAGRAM_DM:
             users = forwarder.get_users()
             context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=forwarder.message_id, text=inform_sending_dms_text)
-            instagram_job.launch_send_dm(targets=users, message=forwarder.telethon_text, forwarder=forwarder, telegram_bot=context.bot)
+            text = forwarder.text
+            text.replace('\\', '')
+            instagram_job.launch_send_dm(targets=users, message=forwarder.text, forwarder=forwarder, telegram_bot=context.bot)
             forwarder.discard()
             return ConversationHandler.END
 
